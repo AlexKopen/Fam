2396cde69dafc8001a308e6852a3aa1f2bd3192f
JamesVorder <jamesvorder@gmail.com>
2018-05-20 16:54:06 -0500

    Basic user registration tests.

--- src/web3/contracts/Registry.sol
+++ src/web3/contracts/Registry.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.4.23;
 
 contract Registry {
+	// IterableMapping.itmap homies;
+	// IterableMapping.itmap budgets;
+	address[] registrants;
 	mapping(address => address[]) public homies;
-	mapping(address => address) public budgets;
+	//mapping(address => address) public budgets;
 
 	constructor() public {
 
@@ -14,11 +17,28 @@ contract Registry {
 			if(homies[_homie1][i] == _homie2) areHomies = true;
 		}
 		require(!areHomies);
-		_; 
+		_;
+	}
+
+	modifier isNotAlreadyRegistered(address _registrant) {
+		require(!isRegistered(_registrant));
+		_;
 	}
 
 
-	function linkUp(address _homie) public areNotAlreadyHomies(msg.sender, _homie){
+	function linkUp(address _homie) public areNotAlreadyHomies(msg.sender, _homie) {
 		homies[msg.sender].push(_homie);
+	}
+
+	function register(address _registrant) public isNotAlreadyRegistered(_registrant) {
+		registrants.push(_registrant);
+	}
+
+	function isRegistered(address _registrant) public view
+	returns(bool) {
+		for(uint i; i < registrants.length; i++) {
+			if(registrants[i] == _registrant) return true;
+		}
+		return false;
 	}
 }
---
+++ src/web3/test/TestRegistry.js
@@ -0,0 +1,21 @@
+var Registry = artifacts.require('Registry');
+
+contract('Registry', function(accounts){
+	it("Should register an account appropriately.", function(){
+		return Registry.deployed().then(function(instance){
+			instance.register(accounts[0]);
+			return instance.isRegistered(accounts[0])
+		});
+	});
+	it("Should not register the same account more than once.", function(){
+		return Registry.deployed().then(function(instance){
+			try{
+				instance.register(accounts[0])
+				assert.fail("Should have failed before adding the account again.");
+			} catch(error) {
+				assert(true);
+			}
+			
+		});
+	});
+});
